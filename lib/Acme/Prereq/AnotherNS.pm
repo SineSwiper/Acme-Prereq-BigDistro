7c5231ab9bb3b6c280de03c5b09e1377def9b740
Brendan Byrd <Perl@ResonatorSoft.org>
2012-07-19 15:44:13 -0400

    Add Acme::Prereq::AnotherNS

---
+++ lib/Acme/Prereq/AnotherNS.pm
@@ -0,0 +1,25 @@
+package Acme::Prereq::AnotherNS;
+
+use strict;
+
+# VERSION
+# ABSTRACT: Module for testing CPAN module prerequisites
+
+42;
+
+__END__
+
+=head1 SYNOPSIS
+ 
+   use Acme::Prereq::BigDistro;
+ 
+=head1 DESCRIPTION
+ 
+Acme::Prereq::BigDistro does nothing.  It's used to test prereqs and 
+emulate a big distribution of modules.
+
+=head1 SEE ALSO
+
+Other Acme::Prereq modules: L<Acme::Prereq::A>, L<Acme::Prereq::B>, L<Acme::Prereq::None>
+
+L<Dist::Zilla::Plugin::PrereqsClean>, which is what actually uses this thing.
